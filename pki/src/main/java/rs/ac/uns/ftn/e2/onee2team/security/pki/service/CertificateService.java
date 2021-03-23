f16505b93d6d06695d4c2f6ead0daa36d36eea07
Igor Šikuljak <41073725+FmasterofU@users.noreply.github.com>
2021-03-23 22:10:49 +0100

    create cert start

--- pki/src/main/java/rs/ac/uns/ftn/e2/onee2team/security/pki/service/CertificateService.java
+++ pki/src/main/java/rs/ac/uns/ftn/e2/onee2team/security/pki/service/CertificateService.java
@@ -4,17 +4,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import rs.ac.uns.ftn.e2.onee2team.security.pki.model.certificate.Certificate;
+import rs.ac.uns.ftn.e2.onee2team.security.pki.model.certificate.CertificateSubject;
 import rs.ac.uns.ftn.e2.onee2team.security.pki.model.certificate.CertificateType;
+import rs.ac.uns.ftn.e2.onee2team.security.pki.model.users.User;
 import rs.ac.uns.ftn.e2.onee2team.security.pki.repository.ICertificateRepository;
 
 @Service
 public class CertificateService implements ICertificateService {
 	
 	private ICertificateRepository certificateRepository;
+	private IUserService userServce;
 	
 	@Autowired
-	public CertificateService(ICertificateRepository certificateRepository) {
+	public CertificateService(ICertificateRepository certificateRepository, IUserService userServce) {
 		this.certificateRepository = certificateRepository;
+		this.userServce = userServce;
 	}
 
 	@Override
@@ -44,5 +48,17 @@ public class CertificateService implements ICertificateService {
 	@Override
 	public Boolean isRevoked(Long serialNumber) {
 		return certificateRepository.findBySerialNumber(serialNumber).getRevoked();
+	}
+	
+	private Certificate createCert(String email, String cn, Long ssn) {
+		Certificate c = new Certificate();
+		User u = userServce.findByEmail(email);
+		Certificate issuer = certificateRepository.findBySerialNumber(ssn);
+		if(u == null) return null;
+		c.setSubject(new CertificateSubject());
+		c.getSubject().setUserSubject(null);
+		c.getSubject().setCommonName(cn);
+		c.setIssuer(issuer.getSubject());
+		return certificateRepository.save(c);
 	}
 }
