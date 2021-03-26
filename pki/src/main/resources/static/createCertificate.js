389c52da32a2100227897a254df79e5fd728dc02
BozidarArsic <arsicbozidar98@gmail.com>
2021-03-26 21:00:30 +0100

    Refactor create method for xml http request

--- pki/src/main/resources/static/createCertificate.js
+++ pki/src/main/resources/static/createCertificate.js
@@ -7,293 +7,257 @@ var pubKey = {}
 
 
 document.addEventListener("DOMContentLoaded", function(event) {
-    collectExtensions();
-    getIssuer();
-    getUsers();
+  collectExtensions();
+  getIssuer();
+  getUsers();
 
-    document.getElementById("create-btn").addEventListener("click", function() { createCertificate() });
-    document.getElementById("type").addEventListener("change", function() { changeType() });
-    document.getElementById("ch-generate-key").addEventListener("change", function() {
-        if (document.getElementById("ch-generate-key").checked) {
-            generatePublicKey();
-        }
-        if (isType("END"))
-            document.getElementById("public-key").disabled = document.getElementById("ch-generate-key").checked ? true : false;
-    });
-    document.getElementById("ch-generate-key").addEventListener("change", function() {
-        if (document.getElementById("ch-generate-key").checked == false)
-            document.getElementById("generated-keys").innerHTML = "";
-    })
-    document.getElementById("update-text-btn").addEventListener("click", function() { updateText(selectedTextId) });
-    document.getElementById("user").addEventListener("change", function() {
-        let email = document.getElementById("user").value;
-        showInTextArea(getUserByEmail(email).userSubject)
-    })
+  document.getElementById("create-btn").addEventListener("click", function() { createCertificate() });
+  document.getElementById("type").addEventListener("change", function() { changeType() });
+  document.getElementById("ch-generate-key").addEventListener("change", function() {
+    if (document.getElementById("ch-generate-key").checked) {
+      generatePublicKey();
+    }
+    if (isType("END"))
+      document.getElementById("public-key").disabled = document.getElementById("ch-generate-key").checked ? true : false;
+  });
+  document.getElementById("ch-generate-key").addEventListener("change", function() {
+    if (document.getElementById("ch-generate-key").checked == false)
+      document.getElementById("generated-keys").innerHTML = "";
+  })
+  document.getElementById("update-text-btn").addEventListener("click", function() { updateText(selectedTextId) });
+  document.getElementById("user").addEventListener("change", function() {
+    let email = document.getElementById("user").value;
+    showInTextArea(getUserByEmail(email).userSubject)
+  })
 });
 
-function generatePublicKey() {
-    let xhr = new XMLHttpRequest();
-
-    xhr.open("POST", "/api/certificates/issuerpubkeys");
-    xhr.setRequestHeader("Authorization", "Bearer " + getJWTToken());
-    xhr.responseType = 'json';
-    xhr.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            populatePublicKeys(this.response);
-        }
-    };
-    xhr.send(document.getElementById('user').value);
+async function generatePublicKey() {
+  HTTPRequest("POST", "/api/certificates/issuerpubkeys", document.getElementById('user').value).then(response => populatePublicKeys(response));
 }
 
 function populatePublicKeys(keys) {
-    let keySelect = document.getElementById("generated-keys");
-    for (let key of keys) {
-        let option = document.createElement('option');
-        option.value = key.publicKey;
-        option.innerText = key.validUntil;
-        keySelect.appendChild(option);
-    }
+  let keySelect = document.getElementById("generated-keys");
+  for (let key of keys) {
+    let option = document.createElement('option');
+    option.value = key.publicKey;
+    option.innerText = key.validUntil;
+    keySelect.appendChild(option);
+  }
 }
 
 function getUserByEmail(email) {
-    for (user of users) {
-        if (user.email == email) return user;
-    }
-    return null;
+  for (user of users) {
+    if (user.email == email) return user;
+  }
+  return null;
 }
 
 function setElementVisibility(element, visible) {
-    if (visible)
-        element.style.display = "block";
-    else
-        element.style.display = "none";;
+  if (visible)
+    element.style.display = "block";
+  else
+    element.style.display = "none";;
 }
 
 function showInTextArea(object) {
-    let info = ""
-    for (let property of Object.keys(object)) {
-        info += property + ": " + object[property] + "\n";
-    }
-    document.getElementById("ta-description").innerText = info;
+  let info = ""
+  for (let property of Object.keys(object)) {
+    info += property + ": " + object[property] + "\n";
+  }
+  document.getElementById("ta-description").innerText = info;
 }
 
 
 async function collectExtensions() {
-    let xhr = new XMLHttpRequest();
 
-    xhr.open("GET", "/api/extensions");
-    xhr.setRequestHeader("Authorization", "Bearer " + getJWTToken());
-    xhr.responseType = 'json';
-    xhr.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            extensions = this.response;
-            populateExtensions();
-        }
-    };
-    xhr.send();
+  HTTPRequest("GET", "/api/extensions")
+    .then(response => {
+      extensions = response;
+      populateExtensions();
+    })
 }
 
 async function getIssuer() {
-    let url = window.location.href;
-    let paramsUrl = url.split('?')[1]
-    let params = paramsUrl.split('&')
-    let issuerSerialId = params[0].split('=')[1];
+  let url = window.location.href;
+  let paramsUrl = url.split('?')[1]
+  let params = paramsUrl.split('&')
+  let issuerSerialId = params[0].split('=')[1];
 
-    let xhr = new XMLHttpRequest();
 
-    xhr.open("GET", "/api/certificates/user/" + issuerSerialId);
-    xhr.setRequestHeader("Authorization", "Bearer " + getJWTToken());
-    xhr.responseType = 'json';
-    xhr.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            issuer = this.response;
-            issuer.id = issuerSerialId
-            let parent = document.getElementById('parent');
-            parent.innerText = issuer.id;
-            parent.addEventListener("click", function() {
-                showInTextArea(issuer);
-            })
-        }
-    };
-    xhr.send();
+
+  HTTPRequest("GET", "/api/certificates/user/" + issuerSerialId)
+    .then(response => {
+      issuer = response;
+      issuer.id = issuerSerialId
+      let parent = document.getElementById('parent');
+      parent.innerText = issuer.id;
+      parent.addEventListener("click", function() {
+        showInTextArea(issuer);
+      })
+    });
+
+
 }
 
 async function getUsers() {
-    let xhr = new XMLHttpRequest();
 
-    xhr.open("GET", "/api/users");
-    xhr.setRequestHeader("Authorization", "Bearer " + getJWTToken());
-    xhr.responseType = 'json';
-    xhr.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            users = this.response;
-            console.log(users);
-            populateUsers();
-        }
-    };
-    xhr.send();
+  HTTPRequest("GET", "/api/users")
+    .then(response => {
+      users = response;
+      console.log(users);
+      populateUsers();
+    });
 }
 
 function populateUsers() {
-    let userSelect = document.getElementById("user");
-    for (let user of users) {
-        let option = document.createElement('option');
-        option.value = user.email;
-        option.innerText = user.email;
-        userSelect.appendChild(option);
-    }
+  let userSelect = document.getElementById("user");
+  for (let user of users) {
+    let option = document.createElement('option');
+    option.value = user.email;
+    option.innerText = user.email;
+    userSelect.appendChild(option);
+  }
 }
 
 
 function changeType() {
-    let pk = document.getElementById("public-key")
-    pk.value = "";
-    pk.disabled = !isType("END");
-    setElementVisibility(document.getElementById("checks"), !isType("ROOT"));
-    setElementVisibility(document.getElementById("generated-keys"), !isType("ROOT"));
-    document.getElementById("ch-generate-key").checked = false;
-    document.getElementById("generated-keys").innerHTML = "";
+  let pk = document.getElementById("public-key")
+  pk.value = "";
+  pk.disabled = !isType("END");
+  setElementVisibility(document.getElementById("checks"), !isType("ROOT"));
+  setElementVisibility(document.getElementById("generated-keys"), !isType("ROOT"));
+  document.getElementById("ch-generate-key").checked = false;
+  document.getElementById("generated-keys").innerHTML = "";
 }
 
 function isType(type) {
-    return document.getElementById("type").value == type
+  return document.getElementById("type").value == type
 }
 
 function createCertificate() {
-    let pubkey;
-    if (isType("ROOT"))
-        pubkey = "";
-    else if (isType("INTERMEDIATE")) {
-        pubkey = document.getElementById("ch-generate-key").checked ? document.getElementById("generated-keys").value : "";
-    } else {
-        pubkey = document.getElementById("ch-generate-key").checked ? document.getElementById("generated-keys").value : document.getElementById("public-key").value;
-    }
-    let request = {
-        type: document.getElementById('type').value,
-        startDate: document.getElementById('start-date').value,
-        endDate: document.getElementById('end-date').value,
-        email: document.getElementById('user').value,
-        commonName: document.getElementById("common-name").value,
-        publicKey: pubkey,
-        issuerSerialNumber: issuer.id,
-        extensions: getUsedExtensions()
-    }
+  let pubkey;
+  if (isType("ROOT"))
+    pubkey = "";
+  else if (isType("INTERMEDIATE")) {
+    pubkey = document.getElementById("ch-generate-key").checked ? document.getElementById("generated-keys").value : "";
+  } else {
+    pubkey = document.getElementById("ch-generate-key").checked ? document.getElementById("generated-keys").value : document.getElementById("public-key").value;
+  }
+  let request = {
+    type: document.getElementById('type').value,
+    startDate: document.getElementById('start-date').value,
+    endDate: document.getElementById('end-date').value,
+    email: document.getElementById('user').value,
+    commonName: document.getElementById("common-name").value,
+    publicKey: pubkey,
+    issuerSerialNumber: issuer.id,
+    extensions: getUsedExtensions()
+  }
 
-    let xhr = new XMLHttpRequest();
-
-    xhr.open("POST", "/api/certificates/create");
-    xhr.setRequestHeader("Authorization", "Bearer " + getJWTToken());
-    xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.responseType = 'json';
-    xhr.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            console.log(this.response)
-        }
-    };
-    xhr.send(JSON.stringify(request));
-
+  HTTPRequest("POST", "/api/certificates/create", request)
+    .then(response => (console.log(response)));
 }
 
 function getUsedExtensions() {
-    let result = []
-    for (let ext of selectedExtensions) {
-        let el = {
-            field: {
-                name: ext.text
-            },
-            value: document.getElementById("ext-data" + ext.id).value
-        }
-        result.push(el)
+  let result = []
+  for (let ext of selectedExtensions) {
+    let el = {
+      field: {
+        name: ext.text
+      },
+      value: document.getElementById("ext-data" + ext.id).value
     }
-    return result;
+    result.push(el)
+  }
+  return result;
 }
 
 
 function populateExtensions() {
-    let table = document.getElementById('availableExtensions');
-    for (let ext of extensions) {
-        let tr = document.createElement('tr');
-        let td = document.createElement('td');
-        td.innerText = ext.name;
-        tr.appendChild(td);
-        table.appendChild(tr);
-    }
+  let table = document.getElementById('availableExtensions');
+  for (let ext of extensions) {
+    let tr = document.createElement('tr');
+    let td = document.createElement('td');
+    td.innerText = ext.name;
+    tr.appendChild(td);
+    table.appendChild(tr);
+  }
 
-    table.addEventListener('click', function(item) {
-        selectRow(item)
-    });
+  table.addEventListener('click', function(item) {
+    selectRow(item)
+  });
 
 }
 
 
 function selectRow(item) {
 
-    var row = item.path[1];
-    let selectedRow;
-    for (var j = 0; j < row.cells.length; j++) {
+  var row = item.path[1];
+  let selectedRow;
+  for (var j = 0; j < row.cells.length; j++) {
 
-        selectedRow = {
-            id: row.rowIndex,
-            text: row.cells[j].innerHTML
-        }
-    };
-    if (row.classList.contains('highlight')) {
-        row.classList.remove('highlight');
-        removeExtensionToCertificate(selectedRow.id);
-        selectedExtensions = removeElementFromList(selectedExtensions, selectedRow);
-    } else {
-        row.classList.add('highlight');
-        selectedExtensions.push(selectedRow)
-        addExtensionToCertificate(selectedRow);
+    selectedRow = {
+      id: row.rowIndex,
+      text: row.cells[j].innerHTML
     }
-    console.log(selectedExtensions)
+  };
+  if (row.classList.contains('highlight')) {
+    row.classList.remove('highlight');
+    removeExtensionToCertificate(selectedRow.id);
+    selectedExtensions = removeElementFromList(selectedExtensions, selectedRow);
+  } else {
+    row.classList.add('highlight');
+    selectedExtensions.push(selectedRow)
+    addExtensionToCertificate(selectedRow);
+  }
+  console.log(selectedExtensions)
 }
 
 function addExtensionToCertificate(item) {
-    let table = document.getElementById('certificate-form');
-    let tr = document.createElement('tr');
-    tr.setAttribute("id", "extension-" + item.id)
+  let table = document.getElementById('certificate-form');
+  let tr = document.createElement('tr');
+  tr.setAttribute("id", "extension-" + item.id)
 
-    let td1 = document.createElement('td');
-    td1.innerText = item.text;
-    tr.appendChild(td1);
+  let td1 = document.createElement('td');
+  td1.innerText = item.text;
+  tr.appendChild(td1);
 
-    let td2 = document.createElement('td');
-    td2.appendChild(createDisabledInputTextWithClickEvent(item.id))
-    tr.appendChild(td2);
-    table.appendChild(tr);
+  let td2 = document.createElement('td');
+  td2.appendChild(createDisabledInputTextWithClickEvent(item.id))
+  tr.appendChild(td2);
+  table.appendChild(tr);
 }
 
 function createDisabledInputTextWithClickEvent(id) {
-    let input = document.createElement('input');
-    input.type = "text"
-    input.setAttribute("id", "ext-data" + id);
-    input.addEventListener("click", function() { addTextToTextArea("ext-data" + id); })
-    return input;
+  let input = document.createElement('input');
+  input.type = "text"
+  input.setAttribute("id", "ext-data" + id);
+  input.addEventListener("click", function() { addTextToTextArea("ext-data" + id); })
+  return input;
 }
 
 function addTextToTextArea(elementId) {
-    let el = document.getElementById(elementId);
-    selectedTextId = elementId;
-    document.getElementById("ta-description").innerText = el.value;
+  let el = document.getElementById(elementId);
+  selectedTextId = elementId;
+  document.getElementById("ta-description").innerText = el.value;
 }
 
 function updateText(selectedTextId) {
-    let el = document.getElementById(selectedTextId);
-    el.value = document.getElementById("ta-description").value;
+  let el = document.getElementById(selectedTextId);
+  el.value = document.getElementById("ta-description").value;
 }
 
 function removeExtensionToCertificate(id) {
-    var elem = document.getElementById('extension-' + id);
-    elem.parentNode.removeChild(elem);
+  var elem = document.getElementById('extension-' + id);
+  elem.parentNode.removeChild(elem);
 }
 
 
 function removeElementFromList(list, item) {
-    for (var i = list.length - 1; i >= 0; i--) {
-        if (list[i].id === item.id) {
-            list.splice(i, 1);
-        }
+  for (var i = list.length - 1; i >= 0; i--) {
+    if (list[i].id === item.id) {
+      list.splice(i, 1);
     }
-    return list;
+  }
+  return list;
 }
--- pki/src/main/resources/static/util.js
+++ pki/src/main/resources/static/util.js
@@ -10,3 +10,25 @@ function dateToDDMMYYYY(dateString) {
   let date = new Date(dateString);
   return date.getDate() + '.' + (date.getMonth() + 1) + '.' + date.getFullYear() + '.';
 }
+
+function HTTPRequest(method, url, params) {
+
+  const promise = new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open(method, url, true);
+    if (params)
+      xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.setRequestHeader("Authorization", "Bearer " + getJWTToken());
+    xhr.responseType = 'json';
+
+    xhr.onreadystatechange = () => {
+      if (xhr.status >= 400)
+        reject(xhr.response);
+      else if (xhr.readyState == 4)
+        resolve(xhr.response);
+    };
+
+    xhr.send(JSON.stringify(params))
+  });
+  return promise;
+}
